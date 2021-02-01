6dbd2886834a04c8f29bb40fc7e3fcae2fdc9354
LEGAGNEUR Matthieu <legagneur.matthieu@gmail.com>
2021-02-01 17:48:23 +0100

    SynDir v1.1 : Fix Windows support

--- SynDir.bat
+++ SynDir.bat
@@ -1,4 +1,4 @@
 @echo off
-rm Copyright 2021 LEGAGNEUR Matthieu
-rm GNU GPL V3 https://www.gnu.org/licenses/gpl.txt
+rem Copyright 2021 LEGAGNEUR Matthieu
+rem GNU GPL V3 https://www.gnu.org/licenses/gpl.txt
 php engine/SynDir.cli.php
--- engine/SynDir.cli.php
+++ engine/SynDir.cli.php
@@ -30,9 +30,9 @@ class SynDirCli {
      * @param boolean $safemode si false : les ficher supprimé en source le seront aussi en destination
      */
     public function __construct($safemode = true) {
-        include_once './cli.class.php';
+        include_once __DIR__.'/cli.class.php';
         while (true) {
-            $SynDir = json_decode(file_get_contents("../config/SynDir.json"), true);
+            $SynDir = json_decode(file_get_contents(__DIR__."/../config/SynDir.json"), true);
             $this_files = [];
             $this_stat = [
                 "A" => 0,
@@ -104,8 +104,8 @@ class SynDirCli {
         $date = new DateTime();
         $datenow = $date->format($format = "YmdHis");
         $datem1m = $date->sub(new DateInterval("P1M"));
-        file_put_contents("../logs/$datenow.log", $log . $this->_stat["log"]);
-        foreach (glob("../logs/*.log") as $log) {
+        file_put_contents(__DIR__."/../logs/$datenow.log", $log . $this->_stat["log"]);
+        foreach (glob(__DIR__."/../logs/*.log") as $log) {
             if ((int) basename($log, ".log") < $datem1m->format($format)) {
                 unlink($log);
             }
